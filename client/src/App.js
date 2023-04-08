76d9f818810a1de71d0b4ac755f90c1affd770ee
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2023-04-07 23:53:53 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import ShuddleRide from './components/ShuddleRide';
 import ShuddleVisit from './components/ShuddleVisit';
 import ShuddleWorld from './components/ShuddleWorld';
 import WorldResults from './components/WorldResults';
 
-const router = createBrowserRouter(
+const router = createHashRouter(
 	[
 		{
 			path: '/',
